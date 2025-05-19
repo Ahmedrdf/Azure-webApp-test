ae10bb1ae43756199aa92ab259a8fe86834f42f8
hmeda <ahmedmansourrdf@outlook.fr>
2025-05-19 22:40:23 +0100

    for testing

---
+++ Azure webApp test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Azure_webApp_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
